92dbc8353ccf754bc3d76467a45f9065421d9da5
baton-oksan <oksanabaton@gmail.com>
2025-01-05 16:43:01 +0400

    refactored ConditionParser

--- QPGradle/src/main/java/org/example/parsers/ConditionParser.java
+++ QPGradle/src/main/java/org/example/parsers/ConditionParser.java
@@ -10,35 +10,43 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ConditionParser implements Parser<List<Condition>> {
-    public List<Condition> parse(String conditionString) {
-        //если строка содержит between, то с помощью регулярки и группы, достать этот between и заменить на пробел
-        //далее сплиттим получившуюся строку по слову AND или OR и получаем каждое условие по отдельности
-        List<Condition> whereList = new ArrayList<>();
-        if (conditionString.contains("between") || conditionString.contains("BETWEEN")) {
-            Pattern betweenPattern = Pattern.compile("(\\S+)\\s+(BETWEEN)\\s+(\\S+\\s+AND\\s+\\S+)\\s+(AND)?");
-            Matcher betweenMatch = betweenPattern.matcher(conditionString);
-            while (betweenMatch.find()) {
-                Condition whereClause = new Condition(betweenMatch.group(1), betweenMatch.group(2), new Source(betweenMatch.group(3)));
-                whereList.add(whereClause);
-            }
-            conditionString = betweenMatch.replaceAll("").trim();
-        }
+    private List<Condition> conditionsList = new ArrayList<>();
 
-        if (conditionString.length() > 3) {
-            String[] whereSplit = conditionString.split(" AND | OR ", Pattern.CASE_INSENSITIVE);
-            for (int i = 0; i < whereSplit.length; i++) {
-                String whereItem = whereSplit[i];
-                String[] whereItemToArray = whereItem.split(" ");
-                //если whereItemToArray[2] содержит  if (fromString.contains("<Subquery_")) , то надо создать объект org.example.Query и засунуть его аргументом в конструктор
-                if (whereItemToArray[2].contains("<Subquery_")) {
-                    String subquery = SQLParser.subqueriesMap.get(whereItemToArray[2]);
-                    Source source = new Source(SQLParser.parseQuery(subquery));
-                    whereList.add(new Condition(whereItemToArray[0], whereItemToArray[1], source));
-                } else
-                    whereList.add(new Condition(whereItemToArray[0], whereItemToArray[1], new Source(whereItemToArray[2])));
-            }
-        }
-        return whereList;
+    public List<Condition> parse(String conditionString) {
+        if (conditionString.contains("between") || conditionString.contains("BETWEEN"))
+              conditionString = parseBetween(conditionString);
+
+        if (conditionString.length() > 3)
+            splitCondition(conditionString);
+
+        return conditionsList;
     }
 
+    private String parseBetween(String conditionString) {
+        //если строка содержит between, то с помощью регулярки и группы, достать этот between и заменить на пробел
+        //далее сплиттим получившуюся строку по слову AND или OR и получаем каждое условие по отдельности
+        Pattern betweenPattern = Pattern.compile("(\\S+)\\s+(BETWEEN)\\s+(\\S+\\s+AND\\s+\\S+)\\s+(AND)?");
+        Matcher betweenMatch = betweenPattern.matcher(conditionString);
+        while (betweenMatch.find()) {
+            Condition conditionClause = new Condition(betweenMatch.group(1), betweenMatch.group(2), new Source(betweenMatch.group(3)));
+            conditionsList.add(conditionClause);
+        }
+        conditionString = betweenMatch.replaceAll("").trim();
+        return conditionString;
+    }
+
+    private void splitCondition(String conditionString) {
+        String[] conditionSplit = conditionString.split(" AND | OR ", Pattern.CASE_INSENSITIVE);
+        for (int i = 0; i < conditionSplit.length; i++) {
+            String conditionItem = conditionSplit[i];
+            String[] conditionItemToArray = conditionItem.split(" ");
+            //если conditionItemToArray[2] содержит  if (fromString.contains("<Subquery_")) , то надо создать объект org.example.Query и засунуть его аргументом в конструктор
+            if (conditionItemToArray[2].contains("<Subquery_")) {
+                String subquery = SQLParser.subqueriesMap.get(conditionItemToArray[2]);
+                Source source = new Source(SQLParser.parseQuery(subquery));
+                conditionsList.add(new Condition(conditionItemToArray[0], conditionItemToArray[1], source));
+            } else
+                conditionsList.add(new Condition(conditionItemToArray[0], conditionItemToArray[1], new Source(conditionItemToArray[2])));
+        }
+    }
 }
